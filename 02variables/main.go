282c7c0f4da2bd1f28f7fde2dfe3ca389560b33c
Supriya Ghorai <supriyagro2001@gmail.com>
2023-12-27 23:59:43 +0530

    first commit day1

---
+++ 02variables/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var username string = "supriya01"
+	fmt.Println(username)
+	fmt.Printf("Variable is type of: %T \n", username)
+
+	var isLoggedIn bool = true
+	fmt.Println(isLoggedIn)
+	fmt.Printf("Variable is type of: %T \n", isLoggedIn)
+
+}
